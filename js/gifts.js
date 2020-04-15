44eba001fae1d2301a0adfb52e972bbc78688715
garn0080 <54903674+garn0080@users.noreply.github.com>
2020-04-15 15:46:38 -0400

    working on postinng gifts

--- js/gifts.js
+++ js/gifts.js
@@ -44,22 +44,77 @@ const app = {
         .then( person => {
             console.log(person)
             let main = document.querySelector('main');
-            if (person.data.gifts.length == 0 ){
+            let giftsArray = person.data.gifts;
+            if ( giftsArray.length == 0 ){
 
                 main.setAttribute('data-status', 'noGiftsYet');
                 let temp = document.getElementById('noGiftsYet');
                 let div = temp.content.cloneNode(true);
                 main.appendChild(div);
-            }else{
-                main.setAttribute('data-status', 'giftAlreadyStored');
-            //     app.personAlreadyStored();
-            //     app.paintList(people);
 
-            //     app.addPerson(); 
-            //     //hacer esto despues si hay personas creadas en la base de datos
+                app.addGift();
+
+            }else{
+
+                main.setAttribute('data-status', 'giftAlreadyStored');
+                let temp = document.getElementById('giftsList');
+                let div = temp.content.cloneNode(true);
+                main.appendChild(div);
+
+                app.addGift();
+           
             }
         })
         .catch(console.error);
+    },
+    addGift: function(){
+
+        //Open modal to create gift
+        var elems = document.querySelectorAll('.modal');
+        M.Modal.init(elems);
+
+        let save = document.getElementById('savebtn');
+        save.addEventListener('click', () => {
+
+            let giftName = document.getElementById('giftName').value;
+            let giftPrice = document.getElementById('price').value;
+            let giftStore = document.getElementById('store').value;
+            let giftWebSite = document.getElementById('webSite').value;
+
+            
+            let num = giftPrice*100;
+            
+            let headers = new Headers();
+            headers.append('Authorization', `Bearer ${app.TOKEN}`);
+            
+            let url = `${app.url}api/people/${app.ID}/gifts`;
+
+            let data = {
+                name: giftName,
+                price: num,
+                imageUrl: "",
+                store: {
+                    name: giftStore,
+                    productURL: giftWebSite
+                },
+            }
+    
+            let req = new Request(url, {
+                headers: headers,
+                mode: 'cors',
+                method: 'POST',
+                body: JSON.stringify(data)
+            });
+    
+            fetch(req)
+            .then(resp => resp.json())
+            .then( gift => {
+                console.log(gift);
+            })
+            .catch(console.error);
+
+        })
+        
     }
 
 
